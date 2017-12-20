59dd3865bba1e668253cd22fa4e98e4fabdb6a9b
jpdbuvic <bouchard.91@gmail.com>
2017-12-20 15:40:14 -0800

    TestTemplate

---
+++ seliniumDemo/src/testpack/TestTemplate.java
@@ -0,0 +1,25 @@
+package testpack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class TestTemplate {
+
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	@After
+	public void tearDown() throws Exception{
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
